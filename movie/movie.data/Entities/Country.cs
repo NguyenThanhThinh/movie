9815f5e3f6636132edc57e904d6a82e8be15f007
ntthinh <ntthinh@cusc.ctu.edu.vn>
2023-03-21 15:13:31 +0900

    refactor entities

--- movie/movie.data/Entities/Country.cs
+++ movie/movie.data/Entities/Country.cs
@@ -1,10 +1,5 @@
 ﻿using movie.data.Constants;
-using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace movie.data.Entities
 {
--- movie/movie.data/Entities/MovieActor.cs
+++ movie/movie.data/Entities/MovieActor.cs
@@ -1,12 +1,6 @@
 ﻿using movie.data.Constants;
-using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Numerics;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace movie.data.Entities
 {
