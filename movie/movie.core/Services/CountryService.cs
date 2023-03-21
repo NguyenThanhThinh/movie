dc118e03c8ed0be4524630b46323ab333e2169e2
ntthinh <ntthinh@cusc.ctu.edu.vn>
2023-03-21 12:45:02 +0900

    update service

--- movie/movie.core/Services/CountryService.cs
+++ movie/movie.core/Services/CountryService.cs
@@ -1,13 +1,31 @@
-﻿using movie.core.Contracts;
+﻿using Microsoft.EntityFrameworkCore;
+using movie.core.Contracts;
 using movie.core.ViewModels.Countries;
+using movie.data.Entities;
+using movie.data.Repositories;
 
-namespace movie.core.Services
+namespace movie.core.Services;
+
+public class CountryService : ICountryService
 {
-    public class CountryService : ICountryService
+    private readonly IRepository<Country> countriesRepository;
+
+    public CountryService(IRepository<Country> countriesRepository)
     {
-        public Task<IEnumerable<CountryViewModel>> GetAllCountriesAsync()
-        {
-            throw new NotImplementedException();
-        }
+        this.countriesRepository = countriesRepository;
+    }
+
+    public async Task<IEnumerable<CountryViewModel>> GetAllCountriesAsync()
+    {
+        var countries = await countriesRepository
+            .AllAsNoTracking()
+            .Select(x => new CountryViewModel()
+            {
+                Id = x.Id,
+                Name = x.Name
+            })
+            .ToListAsync();
+
+        return countries;
     }
 }
